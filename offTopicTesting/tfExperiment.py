e77c94934c9acb72642d5638502b32dd1ae80832
Max Shinnerl <maxshinnerl@gmail.com>
2020-06-02 17:50:27 -0700

    test: trained a test data set (first time)

--- offTopicTesting/tfExperiment.py
+++ offTopicTesting/tfExperiment.py
@@ -30,12 +30,37 @@ train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# ==================================================
+# this block shows the test set with correct labels
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+# ====================================================
+
+
+# set up layers for learning
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+
+# compile the model (loss function, optimizer, metrics)
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# start training model
+model.fit(train_images, train_labels, epochs=10)
+
+# compare to test dataset
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
